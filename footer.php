ceaa10f87c7b52a36d20c4b04fc3d87bbe7e2869
Alari Verev <alariv@users.noreply.github.com>
2016-12-22 13:54:46 +0200

    administrators to footer

--- footer.php
+++ footer.php
@@ -1,2 +1,50 @@
+<?php require ("header.php");?>
+
+<style>
+    hr {
+        display: block;
+        position: relative;
+        padding: 0;
+        margin: 8px auto;
+        margin-top: 300px;
+        height: 0;
+        width: 100%;
+        max-height: 0;
+        font-size: 1px;
+        line-height: 0;
+        clear: both;
+        border: none;
+        border-top: 1px solid #aaaaaa;
+        border-bottom: 1px solid #ffffff;
+    }
+    }
+</style>
+<hr>
+<div class="container">
+    <div class="page-header">
+    </div>
+    <div class="row grid-divider">
+        <div class="col-sm-4 col-md-8 col-sm-offset-2 col-md-offset-2">
+            <center><h3>Administraatorid</h3></center><br>
+                <div class="col-md-5">
+                    <center><h4 style="color: dodgerblue">Alari Verev</h4></center>
+                    <br><br>
+                    <text>telefon:</text><a style="float: right" href="callTo://56601032">56601032</a><br>
+                    <text>skype:</text><a style="float: right" href="skype:automobiil?chat">automobiil</a><br>
+                    <text>kirjuta meil:</text><a style="float: right" href="mailto:alariverev@hotmail.com">alariverev@hotmail.com</a><br>
+
+                </div>
+                <div class="col-md-5 pull-right">
+                    <center><h4 style="color: dodgerblue">Siim Hütsi</h4></center>
+                    <br><br>
+                    <text>telefon:</text><a style="float: right" href="callTo://5036183">5036183</a><br>
+                    <text>skype:</text><a style="float: right" href="skype:siimhutsi?chat">siimhutsi</a><br>
+                    <text>kirjuta meil:</text><a style="float: right" href="mailto:siimh@tlu.ee">siimh@tlu.ee</a><br>
+
+                </div>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
